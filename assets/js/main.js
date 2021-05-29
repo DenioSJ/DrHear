ec6185ca01675dfe0bf2763a94abd57599b8325c
DenioSJ <denioshaji28@gmail.com>
2021-05-29 15:27:08 +0530

    new carousel and about us

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,15 @@
  
   // Carousel
 
+  // let heroCarouselIndicators = select("#hero-carousel-indicators")
+  // let heroCarouselItems = select('#heroCarousel .carousel-item', true)
+
+  // heroCarouselItems.forEach((item, index) => {
+  //   (index === 0) ?
+  //   heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+  //     heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
+  // });
+
   let heroCarouselIndicators = select("#hero-carousel-indicators")
   let heroCarouselItems = select('#heroCarousel .carousel-item', true)
 
@@ -29,6 +38,7 @@
     heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
       heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
   });
+
 
   /**
    * Easy event listener function
